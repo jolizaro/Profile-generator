171b96ebf082c32e2ad989d3df7ad9971b938bfb
Joliza <Jolizaro@gmail.com>
2021-05-27 15:11:50 -0400

    add html functions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
+const generateTeam = require('./lib/generateTeam')
 const fs = require('fs')
 let teamArray = []
 
@@ -40,7 +41,7 @@ function options(){
             type: 'list',
             name: 'userChoice',
             message: 'What would you like to do next?',
-            choices: ['add intern', 'add engineer', 'generate team']
+            choices: ['add manager', 'add intern', 'add engineer', 'generate team']
         },
         
         
@@ -52,19 +53,22 @@ function options(){
           else if (res.userChoice === 'add engineer'){
               addEngineer()
           }
+          else if (res.userChoice === 'add manager'){
+            addManager()
+          }
           else {
-              generateTeam()
+              console.log(generateTeam(teamArray))
           }
         
     })
 
 }
-function askManagerQuestion() {
+function addManager() {
     inquirer.prompt([
         {
             type: 'input',
-            name: 'internName',
-            message: 'What is the interns name?'
+            name: 'managerName',
+            message: 'What is the managers name?'
         },
         {
             type: 'input',
@@ -86,6 +90,67 @@ function askManagerQuestion() {
         const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOfficeNumber)
         teamArray.push(manager)
         options()
+        
     })
 }
-askManagerQuestion()
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the interns name?'
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: 'What is the interns ID ?'
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the interns Email?'
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What is the interns school?'
+        },
+    ])
+    .then (function(res){
+        const intern = new Intern(res.internName, res.internId, res.internEmail, res.internSchool)
+        teamArray.push(intern)
+        options()
+        
+    })
+}
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'EngineerName',
+            message: 'What is the Engineers name?'
+        },
+        {
+            type: 'input',
+            name: 'EngineerId',
+            message: 'What is the Engineers ID ?'
+        },
+        {
+            type: 'input',
+            name: 'EngineerEmail',
+            message: 'What is the Engineers Email?'
+        },
+        {
+            type: 'input',
+            name: 'EngineerGithub',
+            message: 'What is the Engineers Github?'
+        },
+    ])
+    .then (function(res){
+        const engineer = new Engineer(res.EngineerName, res.EngineerId, res.EngineerEmail, res.EngineerGithub)
+        teamArray.push(engineer)
+        options()
+        
+    })
+}
+options()
